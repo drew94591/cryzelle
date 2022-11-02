e755b9a31c070f57413fa2c93e436f83c7bfd244
Andrew Herrera <drew94591@gmail.com>
2022-11-01 18:28:22 -0700

    Added exception handling

--- apps/sms_util.py
+++ apps/sms_util.py
@@ -8,16 +8,15 @@ class SMS:
         account_sid = os.environ['TWILIO_ACCOUNT_SID']
         auth_token = os.environ['TWILIO_AUTH_TOKEN']
         twilio_phone_number = os.environ['TWILIO_PHONE_NUMBER']
-
         client = Client(account_sid, auth_token)
+        placeholder = st.empty()
+        placeholder.empty()
 
         if location == 'main':
             input_phone_number = st.text_input("Enter Phone Number")
             first_name = st.text_input("Friend's First Name")
             if st.button(control_name):
                 try:
-                    st.write(
-                        f"Phone Number: {input_phone_number} for {first_name}")
                     body_msg = (f"Hello {first_name}, {st.session_state['first name']} {st.session_state['last name']} thought you might be interested in joining our network of friends!"
                                 f" Just click this link https://drew94591-cryzelle-register-ytjnoc.streamlitapp.com/ to register @cryptoXchange and receive 100 free Ethereum tokens instantly just for signing up!")
                     to_phone_number = f"+1{input_phone_number}"
@@ -27,17 +26,19 @@ class SMS:
                             from_=twilio_phone_number,
                             to=to_phone_number
                         )
-                    st.write("Message sent successfully!")
                 except:
-                    raise ValueError("You've entered an invalid phone number!")
+                    with placeholder.container():
+                        st.error("You've entered an invalid phone number!")
+                else:
+                    with placeholder.container():
+                        st.info("Message sent successfully!")
+
             elif location == 'sidebar':
                 input_phone_number = st.sidebar.text_input(
                     "Enter Phone Number")
                 first_name = st.text_input("Friend's First Name")
                 if st.sidebar.checkbox(control_name):
                     try:
-                        st.write(
-                            f"Phone Number: {input_phone_number} for {first_name}")
                         body_msg = (f"Hello {first_name}, {st.session_state['first name']} {st.session_state['last name']} thought you might be interested in joining our network of friends!"
                                     f" Just click this link https://drew94591-cryzelle-register-ytjnoc.streamlitapp.com/ to register @cryptoXchange and receive 100 free Ethereum tokens instantly just for signing up!")
                         to_phone_number = f"+1{input_phone_number}"
@@ -47,7 +48,9 @@ class SMS:
                                 from_=twilio_phone_number,
                                 to=to_phone_number
                             )
-                        st.write("Message sent successfully!")
                     except:
-                        raise ValueError(
-                            "You've entered an invalid phone number!")
+                        with placeholder.container():
+                            st.error("You've entered an invalid phone number!")
+                    else:
+                        with placeholder.container():
+                            st.info("Message sent successfully!")
