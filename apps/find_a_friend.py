3eb5ce3b113e5840849b20bd8cc55dc91de33146
ThisGuy <107653555+MC-Stream@users.noreply.github.com>
2022-11-18 16:45:19 -0800

    light changes
    little cleanup

--- apps/find_a_friend.py
+++ apps/find_a_friend.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from streamlit_authenticator.db_utils import get_user_profile_by_phone, get_user_profile_by_email
 
-
 class Friend:
     def search(self):
         st.markdown("# Find a Friend")
@@ -25,7 +24,7 @@ class Friend:
 
                 for i in friend:
                     if i.isnumeric():
-                        phone = phone + i
+                        phone += i
 
                 if len(phone) > 10:
                     if phone[0] == "1":
@@ -56,6 +55,6 @@ class Friend:
                 st.write("Mobile Number: ", friend_information[2])
                 st.write("Email: ", friend_information[3])
                 st.text(" \n")
-
-            else:
+                
+            elif friend_information == None:
                 st.write(f"We could not find {friend} in our database.")
