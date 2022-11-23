c1403413f951628aa319fe34fb08e932e3960339
Theena Dang <maria.cristina.dang@gmail.com>
2022-11-22 22:19:36 -0800

    update the link

--- register.py
+++ register.py
@@ -19,7 +19,7 @@ authenticator = stauth.Authenticate(
 try:
     if authenticator.register_user('Register user', preauthorization=False):
         st.success('User registered successfully!')
-        st.markdown('<a href="https://drew94591-cryzelle-app-xwfwnb.streamlitapp.com" target="_self">Login</a>',
+        st.markdown('<a href="https://chorizoburrito.streamlitapp.com" target="_self">Login</a>',
                     unsafe_allow_html=True)
 except Exception as e:
     st.error(e)
