44125659c474f22f4617e1e117f67807bcff8b11
Theena Dang <maria.cristina.dang@gmail.com>
2022-11-23 00:56:07 -0800

    transaction history get_transaction_count

--- apps/transaction.py
+++ apps/transaction.py
@@ -24,5 +24,5 @@ class Transaction:
         st.write(f'Wallet Address: {wallet_address}')
         st.write(f'Wallet Balance: {ether_amount} ETH')
 
-        st.write(w3.eth.get_block(full_transactions=True))
+        st.write(w3.eth.get_transaction_count(wallet_address))
 
